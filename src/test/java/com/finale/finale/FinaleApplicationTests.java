52d529559a446af9e25c5f97fee8e4160296ae26
seongm1n <0411tjdals34@gmail.com>
2025-10-22 11:41:05 +0900

    initial commit

---
+++ src/test/java/com/finale/finale/FinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finale.finale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
