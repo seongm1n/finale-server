5f8294a2b3f7158ff1ff0586c7ded027d97d176a
seongm1n <0411tjdals34@gmail.com>
2025-10-28 20:24:03 +0900

    Feat: Repository 메서드 추가

--- src/main/java/com/finale/finale/book/repository/QuizRepository.java
+++ src/main/java/com/finale/finale/book/repository/QuizRepository.java
@@ -3,5 +3,5 @@ package com.finale.finale.book.repository;
 import com.finale.finale.book.domain.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuizRepository extends JpaRepository<Quiz, Integer> {
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
 }
