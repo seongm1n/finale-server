dcffeaa519c5d6b2b29c1126bc119ec9c2211357
seongm1n <0411tjdals34@gmail.com>
2025-11-06 16:30:32 +0900

    feat: Implement book pre-generation system
    - Add BookService for book allocation logic
    - Add getNewStory() to retrieve oldest unassigned book
    - Add repository query methods for unassigned books
      - findFirstByUserAndIsProvisionFalseOrderByCreatedAtAsc()
      - countByUserAndIsProvisionFalse()
    - Add new error codes: BOOK_NOT_READY, BOOK_ARE_ENOUGH

--- src/main/java/com/finale/finale/book/repository/BookRepository.java
+++ src/main/java/com/finale/finale/book/repository/BookRepository.java
@@ -1,7 +1,14 @@
 package com.finale.finale.book.repository;
 
+import com.finale.finale.auth.domain.User;
 import com.finale.finale.book.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findFirstByUserAndIsProvisionFalseOrderByCreatedAtAsc(User user);
+
+    int countByUserAndIsProvisionFalse(User user);
 }
--- src/main/java/com/finale/finale/book/repository/QuizRepository.java
+++ src/main/java/com/finale/finale/book/repository/QuizRepository.java
@@ -1,7 +1,12 @@
 package com.finale.finale.book.repository;
 
+import com.finale.finale.book.domain.Book;
 import com.finale.finale.book.domain.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
+
+    List<Quiz> findAllByBook(Book book);
 }
